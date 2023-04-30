716ed774cfa296cd9cd96befe0b8f7dbc8a1b2a7
Reema <reema600@hotmail.com>
2023-04-30 14:13:51 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// step 1
+const exprees = require("express");
+// step 2
+const app = exprees();
+// step 4
+app.get("/name", function (req, res) {
+  res.send("Welcome Reema");
+});
+// step 3
+app.listen(3300, function () {
+  console.log("listening");
+});
