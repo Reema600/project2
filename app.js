ba1e1d5090bd0dbc2e47e837265c278285863251
Reema <reema600@hotmail.com>
2023-05-01 13:48:51 +0300

    adding ejs

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 const exprees = require("express");
 // step 2
 const app = exprees();
+//telling node js to use (ejs) as view engine
+app.set("view engine", "ejs");
 // step 4
 app.get("/name", function (req, res) {
   res.send("Welcome Reema abdullah");
 });
+
+app.get("/testHtml", (req, res) => {
+  res.sendFile(__dirname + "/test.html");
+});
+
+app.get("/hi", (req, res) => {
+  const degree = 25;
+  res.render("introToEjs.ejs", { num: degree });
+});
+
 // step 3
-app.listen(3300, function () {
+app.listen(3000, function () {
   console.log("listening");
 });
